a1e59fb1831f79438df7fd1c3998c621f1edde9a
Salman Farisi <salman@mccollinsmedia.com>
2022-05-26 15:50:35 +0530

    hero banner changes

--- components/HeroBanner.js
+++ components/HeroBanner.js
@@ -1,4 +1,11 @@
-import { Box, Container, Heading, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  Container,
+  Heading,
+  Text,
+} from "@chakra-ui/react";
 import Image from "next/image";
 
 const HeroBanner = () => {
@@ -6,10 +13,9 @@ const HeroBanner = () => {
     <Box
       position="relative"
       className="hello"
-      textAlign="center"
       maxHeight="100vh"
       overflow="hidden"
-      marginTop="-20"
+      marginTop="-110px"
     >
       <Image
         src="/assets/image/banner/home-banner.png"
@@ -17,12 +23,48 @@ const HeroBanner = () => {
         width="1440px"
         height="810px"
         style={{ position: "absolute" }}
+        alt="Mccollins media"
       />
-      <Box position="absolute" bottom="8px" left="16px">
-        <Container>
-          <Heading>digital brand activation agency</Heading>
-          <Text>result driven agency with mobile first technology</Text>
-        </Container>
+      <Box position="absolute" bottom="8%" left="6%">
+        <Heading fontSize="60px" color="#fff">
+          digital brand
+          <br /> activation agency
+        </Heading>
+        <Text fontSize="30px" color="#fff">
+          result driven agency with mobile
+          <br /> first technology
+        </Text>
+        <ButtonGroup gap="4" marginTop="20px" position="relative">
+          <Button
+            background="#FFDE11"
+            colorScheme="yellow"
+            borderRadius="20px"
+            fontWeight="bold"
+            fontSize="20px"
+          >
+            <Image
+              src="/assets/image/navbar/call.png"
+              width="25px"
+              height="25px"
+            />
+            &nbsp;&nbsp;contact us
+          </Button>
+          <Button
+            background="#707070"
+            colorScheme="blackAlpha"
+            borderRadius="20px"
+            color="#fff"
+            fontWeight="bold"
+            fontSize="20px"
+          >
+            <Image
+              src="/assets/image/banner/play.png"
+              width="25px"
+              height="25px"
+            />
+            &nbsp;&nbsp;watch now
+          </Button>
+        </ButtonGroup>
       </Box>
     </Box>
   );
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,15 +1,47 @@
-import { Box, Container, Image } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  Container,
+  Image,
+  Text,
+} from "@chakra-ui/react";
 
 const Navbar = () => {
   return (
-    <Box style={{ position: "relative", zIndex: 99 }}>
-      <Container maxW="7xl">
-        <Box position="relative">
-          <Image src="/assets/image/navbar/logo.png" />
-          hello
-        </Box>
-      </Container>
-    </Box>
+    <header>
+      <Box style={{ position: "relative", zIndex: 99 }} paddingTop="14">
+        <Container maxW="7xl" display="flex">
+          <Box position="relative" flexGrow="1">
+            <Image
+              src="/assets/image/navbar/logo.png"
+              width="223px"
+              height="50px"
+            />
+          </Box>
+          <ButtonGroup gap="4">
+            <Button colorScheme="whiteAlpha" borderRadius="20px" color="#000">
+              <Image
+                src="/assets/image/navbar/call.png"
+                width="25px"
+                height="25px"
+                marginRight="10px"
+              />
+              Call
+            </Button>
+            <Button colorScheme="whiteAlpha" borderRadius="20px" color="#000">
+              <Image
+                src="/assets/image/navbar/chat.png"
+                width="25px"
+                height="25px"
+                marginRight="10px"
+              />
+              Lets talk
+            </Button>
+          </ButtonGroup>
+        </Container>
+      </Box>
+    </header>
   );
 };
 
