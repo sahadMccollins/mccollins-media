3646e19ea33e4a3c63a1a6e157db5c0288f3c167
Salman Farisi <salman@mccollinsmedia.com>
2022-08-27 16:45:31 +0530

    ksa test 4

--- pages/mccollinsmedia-ksa.js
+++ pages/mccollinsmedia-ksa.js
@@ -110,7 +110,6 @@ const MccollinsmediaKsa = () => {
           muted
           width="100%"
           className="video-banner"
-          controls
         >
           <source
             src="https://res.cloudinary.com/mccollins-media/video/upload/v1657219431/Mccollins%20Video/Website_Development_cs3y2v.mp4"
