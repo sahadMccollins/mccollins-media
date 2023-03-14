eb542630318abf9990abeb709eb774c319c130e1
Salman Farisi <salman@mccollinsmedia.com>
2023-03-14 13:28:50 +0400

    13-03-23 11 fix

--- pages/_document.js
+++ pages/_document.js
@@ -4,21 +4,19 @@ import { useRouter } from "next/router";
 
 export default function Document() {
   const router = useRouter();
-  if (router.pathname.includes("/admin")) {
-    useEffect(() => {
-      (function (w, d, s, l, i) {
-        w[l] = w[l] || [];
-        w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" });
-        var f = d.getElementsByTagName(s)[0],
-          j = d.createElement(s),
-          dl = l != "dataLayer" ? "&l=" + l : "";
-        j.async = true;
-        j.defer = true;
-        j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
-        f.parentNode.insertBefore(j, f);
-      })(window, document, "script", "dataLayer", "GTM-KK6CH2S");
-    }, []);
-  }
+  // useEffect(() => {
+  //   (function (w, d, s, l, i) {
+  //     w[l] = w[l] || [];
+  //     w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" });
+  //     var f = d.getElementsByTagName(s)[0],
+  //       j = d.createElement(s),
+  //       dl = l != "dataLayer" ? "&l=" + l : "";
+  //     j.async = true;
+  //     j.defer = true;
+  //     j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
+  //     f.parentNode.insertBefore(j, f);
+  //   })(window, document, "script", "dataLayer", "GTM-KK6CH2S");
+  // }, []);
   return (
     <Html>
       <Head>
