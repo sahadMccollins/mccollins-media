baae6c0a75f8bd027332ca6c4a8d14fca2dd8e50
Salman Farisi <salman@mccollinsmedia.com>
2023-03-14 13:37:07 +0400

    13-03-23 12

--- pages/_app.js
+++ pages/_app.js
@@ -140,6 +140,20 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   const { title, description } = metaData[router.pathname.toString()] || {};
 
   useEffect(() => {
+    (function (w, d, s, l, i) {
+      w[l] = w[l] || [];
+      w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" });
+      var f = d.getElementsByTagName(s)[0],
+        j = d.createElement(s),
+        dl = l != "dataLayer" ? "&l=" + l : "";
+      j.async = true;
+      j.defer = true;
+      j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
+      f.parentNode.insertBefore(j, f);
+    })(window, document, "script", "dataLayer", "GTM-KK6CH2S");
+  }, []);
+
+  useEffect(() => {
     TagManager.initialize({ gtmId: "GTM-KK6CH2S" });
     router.events.on("routeChangeComplete", (url) => {
       TagManager.dataLayer({
