29364e7a89c39226d1402e5aa90b4e516dc3da03
de-salman <salmanfarisi.bca@gmail.com>
2023-08-16 16:14:34 +0530

    16-08 2

--- pages/film-production.js
+++ pages/film-production.js
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
 import EmptyLayout from "../components/Layout/EmptyLayout";
 
 const FilmProduction = () => {
+  useEffect(() => {
+    // Add the Google Tag Manager script to this page
+    const script = document.createElement("script");
+    script.src = "https://www.googletagmanager.com/gtag/js?id=AW-10803441186";
+    script.async = true;
+    document.head.appendChild(script);
+
+    script.onload = () => {
+      window.dataLayer = window.dataLayer || [];
+      function gtag() {
+        window.dataLayer.push(arguments);
+      }
+      gtag("js", new Date());
+      gtag("config", "AW-10803441186");
+    };
+
+    return () => {
+      // Clean up the script when the component unmounts
+      document.head.removeChild(script);
+    };
+  }, []);
+
   return (
     <>
-      <iframe
-        src="https://film.mccollinsmedia.com"
+      <embed
+        src="https://film.mccollinsmedia.com/"
         title="Film Production"
         style={{ width: "100%", height: "100vh", border: "none" }}
       />
