276267f821ed9416f26b0237e3bdd4756da40054
de-salman <salmanfarisi.bca@gmail.com>
2023-09-05 13:34:21 +0530

    fix banner margin

--- pages/landing-pages/web-design-development-1.js
+++ pages/landing-pages/web-design-development-1.js
@@ -1,11 +1,11 @@
-
 import React, { useEffect, useRef } from "react";
 import MainBanner1 from "../../components/Landing-page-components/MainBanner";
+import { Box } from "@chakra-ui/react";
 const LandingPage1 = () => {
-    return(
-        <div>
-            <MainBanner1/>
-        </div>
-    )
-}
+  return (
+    <Box marginTop={"-85px"} position="relative">
+      <MainBanner1 />
+    </Box>
+  );
+};
 export default LandingPage1;
