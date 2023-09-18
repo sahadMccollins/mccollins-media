e39e016ad78f0d66920b0ed4c128384067d95500
jbeanIjas <32436988+jbeanIjas@users.noreply.github.com>
2023-09-18 13:52:52 +0530

    555

--- components/Landing-page-components/PerfomanceSlider1.js
+++ components/Landing-page-components/PerfomanceSlider1.js
@@ -6,10 +6,12 @@ import "swiper/css";
 import dynamic from "next/dynamic";
 const ModalVideo = dynamic(() => import("react-modal-video"), { ssr: false });
 import "../../node_modules/react-modal-video/css/modal-video.min.css";
+import { useMediaQuery } from "@chakra-ui/react";
 
 const PerfomanceSlider1 = () => {
-    const swiperRef = useRef(null);
-    const [isOpen, setOpen] = useState(false);
+  const swiperRef = useRef(null);
+  const [isOpen, setOpen] = useState(false);
+  const [isSmallerThan1000] = useMediaQuery("(max-width: 1000px)");
   return (
     <div className="PerfomanceWrap pd-common-container">
       <div className="icontainer pt-0">
@@ -30,102 +32,136 @@ const PerfomanceSlider1 = () => {
                 3 Top performing Social Media case studies of our clients
               </div>
               <div className="irow">
+                {isSmallerThan1000 && (
+                  <div className="PrGridMiddle2">
+                    <div
+                      className="VideoModalClick"
+                      onClick={() => setOpen(true)}
+                    >
+                      <Image
+                        alt="McCollins Media"
+                        src="/lp/video.jpg"
+                        width={"662px"}
+                        height={"330px"}
+                        layout="responsive"
+                      />
+                    </div>
+                    <ModalVideo
+                      channel="youtube"
+                      autoplay
+                      isOpen={isOpen}
+                      videoId="Pj6P8kQvpZw"
+                      onClose={() => setOpen(false)}
+                    />
+                  </div>
+                )}
                 <div className="PrGridMiddle1">
                   <div className="SwiperSliderWarp ">
                     <Swiper
-                     spaceBetween={20}
-                     slidesPerView={1}
-                     loop={true}
-                     ref={swiperRef}
+                      spaceBetween={20}
+                      slidesPerView={1}
+                      loop={true}
+                      ref={swiperRef}
                     >
                       <SwiperSlide>
                         <div className="PerfomanceGraphWrap">
-                            <div className="PerfomanceG">
-                                <div className="PerfomanceG1">1234567</div>
-                                <div className="PerfomanceG2">Spend</div>
-                            </div>
-                            <div className="PerfomanceG">
-                                <div className="PerfomanceG1">7890123</div>
-                                <div className="PerfomanceG2">Sales</div>
-                            </div>
-                            <div className="PerfomanceG">
-                                <div className="PerfomanceG1">8701</div>
-                                <div className="PerfomanceG2">Orders</div>
-                            </div>
-                            <div className="PerfomanceG">
-                                <div className="PerfomanceG1">4.8X</div>
-                                <div className="PerfomanceG2">Roas</div>
-                            </div>
+                          <div className="PerfomanceG">
+                            <div className="PerfomanceG1">1234567</div>
+                            <div className="PerfomanceG2">Spend</div>
+                          </div>
+                          <div className="PerfomanceG">
+                            <div className="PerfomanceG1">7890123</div>
+                            <div className="PerfomanceG2">Sales</div>
+                          </div>
+                          <div className="PerfomanceG">
+                            <div className="PerfomanceG1">8701</div>
+                            <div className="PerfomanceG2">Orders</div>
+                          </div>
+                          <div className="PerfomanceG">
+                            <div className="PerfomanceG1">4.8X</div>
+                            <div className="PerfomanceG2">Roas</div>
+                          </div>
                         </div>
                       </SwiperSlide>
                       <SwiperSlide>
                         <div className="PerfomanceGraphWrap">
-                            <div className="PerfomanceG">
-                                <div className="PerfomanceG1">1234567</div>
-                                <div className="PerfomanceG2">Spend</div>
-                            </div>
-                            <div className="PerfomanceG">
-                                <div className="PerfomanceG1">7890123</div>
-                                <div className="PerfomanceG2">Sales</div>
-                            </div>
-                            <div className="PerfomanceG">
-                                <div className="PerfomanceG1">8701</div>
-                                <div className="PerfomanceG2">Orders</div>
-                            </div>
-                            <div className="PerfomanceG">
-                                <div className="PerfomanceG1">4.8X</div>
-                                <div className="PerfomanceG2">Roas</div>
-                            </div>
+                          <div className="PerfomanceG">
+                            <div className="PerfomanceG1">1234567</div>
+                            <div className="PerfomanceG2">Spend</div>
+                          </div>
+                          <div className="PerfomanceG">
+                            <div className="PerfomanceG1">7890123</div>
+                            <div className="PerfomanceG2">Sales</div>
+                          </div>
+                          <div className="PerfomanceG">
+                            <div className="PerfomanceG1">8701</div>
+                            <div className="PerfomanceG2">Orders</div>
+                          </div>
+                          <div className="PerfomanceG">
+                            <div className="PerfomanceG1">4.8X</div>
+                            <div className="PerfomanceG2">Roas</div>
+                          </div>
                         </div>
                       </SwiperSlide>
                       <SwiperSlide>
                         <div className="PerfomanceGraphWrap">
-                            <div className="PerfomanceG">
-                                <div className="PerfomanceG1">1234567</div>
-                                <div className="PerfomanceG2">Spend</div>
-                            </div>
-                            <div className="PerfomanceG">
-                                <div className="PerfomanceG1">7890123</div>
-                                <div className="PerfomanceG2">Sales</div>
-                            </div>
-                            <div className="PerfomanceG">
-                                <div className="PerfomanceG1">8701</div>
-                                <div className="PerfomanceG2">Orders</div>
-                            </div>
-                            <div className="PerfomanceG">
-                                <div className="PerfomanceG1">4.8X</div>
-                                <div className="PerfomanceG2">Roas</div>
-                            </div>
+                          <div className="PerfomanceG">
+                            <div className="PerfomanceG1">1234567</div>
+                            <div className="PerfomanceG2">Spend</div>
+                          </div>
+                          <div className="PerfomanceG">
+                            <div className="PerfomanceG1">7890123</div>
+                            <div className="PerfomanceG2">Sales</div>
+                          </div>
+                          <div className="PerfomanceG">
+                            <div className="PerfomanceG1">8701</div>
+                            <div className="PerfomanceG2">Orders</div>
+                          </div>
+                          <div className="PerfomanceG">
+                            <div className="PerfomanceG1">4.8X</div>
+                            <div className="PerfomanceG2">Roas</div>
+                          </div>
                         </div>
                       </SwiperSlide>
                     </Swiper>
                   </div>
                 </div>
-                <div className="PrGridMiddle2">
-                  <div className="VideoModalClick" onClick={() => setOpen(true)}>
-                    <Image
-                      alt="McCollins Media"
-                      src="/lp/video.jpg"
-                      width={"662px"}
-                      height={"330px"}
-                      layout="responsive"
+                {!isSmallerThan1000 && (
+                  <div className="PrGridMiddle2">
+                    <div
+                      className="VideoModalClick"
+                      onClick={() => setOpen(true)}
+                    >
+                      <Image
+                        alt="McCollins Media"
+                        src="/lp/video.jpg"
+                        width={"662px"}
+                        height={"330px"}
+                        layout="responsive"
+                      />
+                    </div>
+                    <ModalVideo
+                      channel="youtube"
+                      autoplay
+                      isOpen={isOpen}
+                      videoId="Pj6P8kQvpZw"
+                      onClose={() => setOpen(false)}
                     />
                   </div>
-                  <ModalVideo
-          channel="youtube"
-          autoplay
-          isOpen={isOpen}
-          videoId="Pj6P8kQvpZw"
-          onClose={() => setOpen(false)}
-        />
-                </div>
+                )}
               </div>
               <div className="SliderBtnWrp">
-                <span className="SliderBtn NextBtn" onClick={() => swiperRef.current.swiper.slideNext()}>
-                    <span className="Ner1">&nbsp;</span>
+                <span
+                  className="SliderBtn NextBtn"
+                  onClick={() => swiperRef.current.swiper.slideNext()}
+                >
+                  <span className="Ner1">&nbsp;</span>
                 </span>
-                <span className="SliderBtn PrevBtn" onClick={() => swiperRef.current.swiper.slidePrev()}>
-                <span className="Ner2">&nbsp;</span>
+                <span
+                  className="SliderBtn PrevBtn"
+                  onClick={() => swiperRef.current.swiper.slidePrev()}
+                >
+                  <span className="Ner2">&nbsp;</span>
                 </span>
               </div>
             </div>
