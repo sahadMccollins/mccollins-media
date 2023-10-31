a5e151c3879e9ac2c535e1a97b97c25af29ca8a8
de-salman <salmanfarisi.bca@gmail.com>
2023-10-31 12:16:52 +0530

    3-10-23

--- pages/index.js
+++ pages/index.js
@@ -19,6 +19,12 @@ export default function Home({ metaTags }) {
         {metaTags.length > 0 &&
           metaTags[0].content &&
           ReactHtmlParser(metaTags[0].content)}
+        <script
+          type="text/javascript"
+          src="https://crm.zoho.com/crm/javascript/zcga.js"
+        >
+           
+        </script>
       </Head>
       <HeroBanner
         img="/assets/cloudinaryBackup/home-banner_bwes6c.webp"
