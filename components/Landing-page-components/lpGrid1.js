2c83f0730c98dd08efb068b4e5337026617b9811
jbeanIjas <32436988+jbeanIjas@users.noreply.github.com>
2024-05-01 10:43:54 +0530

    ij

--- components/Landing-page-components/lpGrid1.js
+++ components/Landing-page-components/lpGrid1.js
@@ -5,207 +5,88 @@ import "react-intl-tel-input/dist/main.css";
 const LpGrid1 = () => {
   return (
     <div className="LpGridBoxWrp LpGridBoxWrp1">
-      <div className="LpGridBox pd-common bg1">
+      <div className="LpGridBox pd-common ">
         <div className="icontainer">
-          <div className="Row2">
-            <div className="c5">
-                <div className="TExtHeaderWrap">
-                    <div className="TExtHeader">
-                        <div className="TExtHeaderL">
-                            1
-                        </div>
-                        <div className="TExtHeaderR">Metaverse</div>
-                    </div>
-                </div>
-                <div className="T8">Our corporate website development services unlock the potential of your corporate identity. Our expertly tailored websites are designed to reflect your unique brand and values, offering seamless user experiences across all devices</div>
-                
-            </div>
-            <div className="c55">
-             <div className="ImageWrp1">
-             <Image
-                src="/cgi/k1.jpg"
-                layout="responsive"
-                width={"1000"}
-                height={"611"}
-                priority={true}
-              />
-             </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="LpGridBox pd-common bg2">
-        <div className="icontainer">
-          <div className="Row2">
-          <div className="c555">
-          <div className="ImageWrp1">
-          <Image
-                src="/cgi/k2.jpg"
-                layout="responsive"
-                width={"1000"}
-                height={"611"}
-                priority={true}
-              />
-             </div>
-            </div>
-            <div className="c5555">
-                <div className="TExtHeaderWrap">
-                    <div className="TExtHeader">
-                        <div className="TExtHeaderL">
-                            2
-                        </div>
-                        <div className="TExtHeaderR">AR Glasses</div>
-                    </div>
-                </div>
-                <div className="T8">Our E-commerce website development services follow a strategic approach to design and functionality, guaranteeing a seamless shopping experience for your customers. This results in higher conversion rates and increased revenue for your business</div>
-            </div>
-            
-          </div>
-        </div>
-      </div>
-      <div className="LpGridBox pd-common bg1">
-        <div className="icontainer">
-          <div className="Row2">
-            <div className="c5">
-                <div className="TExtHeaderWrap">
-                    <div className="TExtHeader">
-                        <div className="TExtHeaderL">
-                            3
-                        </div>
-                        <div className="TExtHeaderR">Extended reality</div>
-                    </div>
-                </div>
-                <div className="T8">Our corporate website development services unlock the potential of your corporate identity. Our expertly tailored websites are designed to reflect your unique brand and values, offering seamless user experiences across all devices</div>
-                
-            </div>
-            <div className="c55">
-             <div className="ImageWrp1">
-             <Image
-                src="/cgi/k3.jpg"
-                layout="responsive"
-                width={"1000"}
-                height={"611"}
-                priority={true}
-              />
-             </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="LpGridBox pd-common bg2">
-        <div className="icontainer">
-          <div className="Row2">
-          <div className="c555">
-          <div className="ImageWrp1">
-          <Image
+        <div className="row1">
+            <div className="col-md-4 FtrClmn">
+            <Image
                 src="/cgi/k4.jpg"
                 layout="responsive"
                 width={"1000"}
                 height={"611"}
                 priority={true}
               />
-             </div>
+              <div class="TExtHeaderR">Augmented Reality</div>
+              <div class="TExtHeaderR1">Enhance reality with digital overlays.</div>
             </div>
-            <div className="c5555">
-                <div className="TExtHeaderWrap">
-                    <div className="TExtHeader">
-                        <div className="TExtHeaderL">
-                            4
-                        </div>
-                        <div className="TExtHeaderR">Augmented Reality</div>
-                    </div>
-                </div>
-                <div className="T8">Our E-commerce website development services follow a strategic approach to design and functionality, guaranteeing a seamless shopping experience for your customers. This results in higher conversion rates and increased revenue for your business</div>
-            </div>
-            
-          </div>
-        </div>
-      </div>
-      <div className="LpGridBox pd-common bg1">
-        <div className="icontainer">
-          <div className="Row2">
-            <div className="c5">
-                <div className="TExtHeaderWrap">
-                    <div className="TExtHeader">
-                        <div className="TExtHeaderL">
-                            5
-                        </div>
-                        <div className="TExtHeaderR">AI photobooth</div>
-                    </div>
-                </div>
-                <div className="T8">Our corporate website development services unlock the potential of your corporate identity. Our expertly tailored websites are designed to reflect your unique brand and values, offering seamless user experiences across all devices</div>
-                
-            </div>
-            <div className="c55">
-             <div className="ImageWrp1">
-             <Image
-                src="/cgi/k5.jpg"
-                layout="responsive"
-                width={"1000"}
-                height={"611"}
-                priority={true}
-              />
-             </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="LpGridBox pd-common bg2">
-        <div className="icontainer">
-          <div className="Row2">
-          <div className="c555">
-          <div className="ImageWrp1">
-          <Image
-                src="/cgi/k6.jpg"
-                layout="responsive"
-                width={"1000"}
-                height={"611"}
-                priority={true}
-              />
-             </div>
-            </div>
-            <div className="c5555">
-                <div className="TExtHeaderWrap">
-                    <div className="TExtHeader">
-                        <div className="TExtHeaderL">
-                            6
-                        </div>
-                        <div className="TExtHeaderR">AI Chatbot</div>
-                    </div>
-                </div>
-                <div className="T8">Our E-commerce website development services follow a strategic approach to design and functionality, guaranteeing a seamless shopping experience for your customers. This results in higher conversion rates and increased revenue for your business</div>
-            </div>
-            
-          </div>
-        </div>
-      </div>
-      <div className="LpGridBox pd-common bg1">
-        <div className="icontainer">
-          <div className="Row2">
-            <div className="c5">
-                <div className="TExtHeaderWrap">
-                    <div className="TExtHeader">
-                        <div className="TExtHeaderL">
-                            7
-                        </div>
-                        <div className="TExtHeaderR">Virtual Reality</div>
-                    </div>
-                </div>
-                <div className="T8">Our corporate website development services unlock the potential of your corporate identity. Our expertly tailored websites are designed to reflect your unique brand and values, offering seamless user experiences across all devices</div>
-                
-            </div>
-            <div className="c55">
-             <div className="ImageWrp1">
-             <Image
+            <div className="col-md-4 FtrClmn">
+            <Image
                 src="/cgi/k7.jpg"
                 layout="responsive"
                 width={"1000"}
                 height={"611"}
                 priority={true}
               />
-             </div>
+              <div class="TExtHeaderR">Virtual Reality</div>
+              <div class="TExtHeaderR1">Immerse in simulated environments.</div>
+            </div>
+            <div className="col-md-4 FtrClmn">
+            <Image
+                src="/cgi/k10.jpg"
+                layout="responsive"
+                width={"1000"}
+                height={"611"}
+                priority={true}
+              />
+              <div class="TExtHeaderR">Metaverse</div>
+              <div class="TExtHeaderR1">Connect in a shared digital universe.</div>
+            </div>
+            <div className="col-md-4 FtrClmn">
+            <Image
+                src="/cgi/k2.jpg"
+                layout="responsive"
+                width={"1000"}
+                height={"611"}
+                priority={true}
+              />
+              <div class="TExtHeaderR">AR Glasses</div>
+              <div class="TExtHeaderR1">Wearable devices for augmented reality experiences.</div>
+            </div>
+            <div className="col-md-4 FtrClmn">
+            <Image
+                src="/cgi/k3.jpg"
+                layout="responsive"
+                width={"1000"}
+                height={"611"}
+                priority={true}
+              />
+              <div class="TExtHeaderR">Extended Reality</div>
+              <div class="TExtHeaderR1">Merge virtual and physical worlds seamlessly.</div>
+            </div>
+            <div className="col-md-4 FtrClmn">
+            <Image
+                src="/cgi/k6.jpg"
+                layout="responsive"
+                width={"1000"}
+                height={"611"}
+                priority={true}
+              />
+              <div class="TExtHeaderR">AI Chatbot</div>
+              <div class="TExtHeaderR1">Intelligent virtual conversational assistant.</div>
+            </div>
+            <div className="col-md-4 FtrClmn">
+            <Image
+                src="/cgi/k5.jpg"
+                layout="responsive"
+                width={"1000"}
+                height={"611"}
+                priority={true}
+              />
+              <div class="TExtHeaderR">AI Photobooth</div>
+              <div class="TExtHeaderR1">Automated photo experience with AI enhancements.</div>
             </div>
           </div>
+          
         </div>
       </div>
      
