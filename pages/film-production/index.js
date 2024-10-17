9e83c6a20a7a8ddc8d53bed716b67a4f913eada6
de-salman <salman@mccollinsmedia.com>
2024-10-17 12:41:51 +0530

    fil prd issue fix 3

--- pages/film-production/index.js
+++ pages/film-production/index.js
@@ -108,7 +108,7 @@ const FilmProduction = () => {
           duration: 9000,
           isClosable: true,
         })
-        // router.push(`/film-production/thank-you yes`)
+        // router.push(`/film-production/thank-you`)
       );
     } else {
       toast({
