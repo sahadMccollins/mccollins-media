5df94096a20a2017e278c1773b8c4d0a797f88a6
McCollinsMediaWeb <111267011+McCollinsMediaWeb@users.noreply.github.com>
2025-10-01 11:39:38 +0530

    55

--- pages/landing-pages/social-media-marketing.js
+++ pages/landing-pages/social-media-marketing.js
@@ -9,6 +9,7 @@ import WhatWeOffer1 from "../../components/Landing-page-components/WhatWeOffer";
 import ClientResults from "../../components/Landing-page-components/ClientResults";
 import OurPartners1 from "../../components/Landing-page-components/OurPartners1";
 import Accordion1 from "../../components/Landing-page-components/Accordion1";
+import BannerBox from "../../components/BanneBox";
 const LandingPage1 = () => {
   useEffect(() => {
     document.body.className = "LandingPage";
@@ -24,8 +25,53 @@ const LandingPage1 = () => {
       </Head>
       <SocialMainBanner />
       <ClientPortFolio />
-      <WhatWeOffer1 />
       
+      <WhatWeOffer1 />
+      <BannerBox
+                Banner="/services/social/McC_SocMedMark_Web_1-min.jpg"
+                MobileBanner="/services/web/McC_SocMedMark_Mob_1-min.jpg"
+              />
+              <BannerBox
+                Banner="/services/social/McC_SocMedMark_Web_2-min.jpg"
+                MobileBanner="/services/web/McC_SocMedMark_Mob_2-min.jpg"
+              />
+              <BannerBox
+                Banner="/services/social/McC_SocMedMark_Web_3-min.jpg"
+                MobileBanner="/services/web/McC_SocMedMark_Mob_3-min.jpg"
+              />
+              <BannerBox
+                Banner="/services/social/McC_SocMedMark_Web_4-min.jpg"
+                MobileBanner="/services/web/McC_SocMedMark_Mob_4-min.jpg"
+              />
+              <BannerBox
+                Banner="/services/social/McC_SocMedMark_Web_5-min.jpg"
+                MobileBanner="/services/web/McC_SocMedMark_Mob_5-min.jpg"
+              />
+              <BannerBox
+                Banner="/services/social/McC_SocMedMark_Web_6-min.jpg"
+                MobileBanner="/services/web/McC_SocMedMark_Mob_6-min.jpg"
+              />
+              <BannerBox
+                Banner="/services/social/McC_SocMedMark_Web_7-min.jpg"
+                MobileBanner="/services/web/McC_SocMedMark_Mob_7-min.jpg"
+              />
+              <BannerBox
+                Banner="/services/social/McC_SocMedMark_Web_8-min.jpg"
+                MobileBanner="/services/web/McC_SocMedMark_Mob_8-min.jpg"
+              />
+              <BannerBox
+                Banner="/services/social/McC_SocMedMark_Web_9-min.jpg"
+                MobileBanner="/services/web/McC_SocMedMark_Mob_9-min.jpg"
+              />
+              <BannerBox
+                Banner="/services/social/McC_SocMedMark_Web_10-min.jpg"
+                MobileBanner="/services/web/McC_SocMedMark_Mob_10-min.jpg"
+              />
+              <BannerBox
+                Banner="/services/social/McC_SocMedMark_Web_11-min.jpg"
+                MobileBanner="/services/web/McC_SocMedMark_Mob_11-min.jpg"
+              />
+
       <OurPartners1 />
       <ClientResults />
       <Accordion1/>
